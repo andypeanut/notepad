586afb41c4fde88012edf334ec8bf5fa16c172c0
andypeanut <35947732+andypeanut@users.noreply.github.com>
2022-05-30 13:45:52 +0900

    Add files via upload

---
+++ NotepadApp/NotepadApp/Form1.cs
@@ -0,0 +1,113 @@
+namespace NotepadApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void 새로만들기ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            tbContents.Text = "";
+        }
+
+        private void 열기ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //사용자에게 열 파일을 선택 하게함
+            openFileDialog1.FileName = "";
+            openFileDialog1.Filter = "텍스트 문서(*.txt)|*.txt|모든파일|*.*";
+            openFileDialog1.ShowDialog();
+            if (openFileDialog1.FileName == "")
+            {
+            }
+            else
+            {
+                tbContents.Text = System.IO.File.ReadAllText(openFileDialog1.FileName);
+                this.Text = "메모장 :" + openFileDialog1.FileName;
+            }
+        }
+
+        private void 저장ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            {
+                if (saveFileDialog1.FileName == "")
+                {
+                    saveFileDialog1.Filter = "텍스트 문서(*.txt)|*.txt|모든파일|*.*";
+                    saveFileDialog1.ShowDialog();
+                    if (saveFileDialog1.FileName == "")
+                    {
+                    }
+                    else
+                    {
+                        System.IO.File.WriteAllText(saveFileDialog1.FileName, tbContents.Text);
+                        //파일을 저장
+                        saveFileDialog1.FileName = "";
+                    }
+                }
+                else
+                {
+                    System.IO.File.WriteAllText(saveFileDialog1.FileName, tbContents.Text);
+                }
+            }
+        }
+
+        private void 다른이름으로저장ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "텍스트 문서(*.txt)|*.txt|모든파일|*.*";
+            saveFileDialog1.ShowDialog();
+            //사용자에게 저장할 파일을 선택하게함
+            if (saveFileDialog1.FileName != "")
+            {
+                System.IO.File.WriteAllText(saveFileDialog1.FileName, tbContents.Text);
+                saveFileDialog1.FileName = "";
+            }
+            //파일을 저장
+            else
+            {
+
+            }
+
+        }
+
+        private void 끝내기ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+            //프로그램 종료
+        }
+
+        private void 자동줄바꿈ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            tbContents.WordWrap = !tbContents.WordWrap;
+        }
+
+        private void 글꼴ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            fontDialog1.ShowDialog();
+            tbContents.Font = fontDialog1.Font; 
+        }
+
+        private void 상태표시줄ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            statusStrip1.Visible = !statusStrip1.Visible;
+        }
+
+        private void 메모장정보ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.ShowDialog();
+        }
+
+        private void 배경색ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            tbContents.BackColor = colorDialog1.Color;
+        }
+
+        private void tbContents_TextChanged(object sender, EventArgs e)
+        {
+            Line.Text = "줄수 : " + tbContents.Lines.Length;
+            Line2.Text = "단어수 : " + tbContents.Text.Split(" ").Length;
+        }
+    }
+}
---
+++ NotepadApp/NotepadApp/Form2.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace NotepadApp
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(294, 136);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(194, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "이 화면은 도움 화면 입니다";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(294, 228);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(194, 81);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "확인";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button button1;
+    }
+}
